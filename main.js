ef711005b336b40753a03f3d3f45984fb78e7562
sd8917 <sudhanshuraj8917@gmail.com>
2021-07-03 18:54:01 +0530

    video playerupdated

--- main.js
+++ main.js
@@ -1,113 +1,220 @@
-console.log("script loaded");
 
-/*API DATA 
-*/
-var productData = {
-    "id": "1",
-    "name": "Men Navy Blue Solid Sweatshirt",
-    "preview": "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/7579188/2018/11/5/08a7b230-ee8f-46c0-a945-4e835a3c01c01541402833619-United-Colors-of-Benetton-Men-Sweatshirts-1271541402833444-1.jpg",
-    "photos": [
-      "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/7579188/2018/11/5/08a7b230-ee8f-46c0-a945-4e835a3c01c01541402833619-United-Colors-of-Benetton-Men-Sweatshirts-1271541402833444-1.jpg",
-      "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/7579188/2018/11/5/efc3d5b9-1bb3-4427-af53-7acae7af98951541402833591-United-Colors-of-Benetton-Men-Sweatshirts-1271541402833444-2.jpg",
-      "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/7579188/2018/11/5/c7e58861-3431-4189-9903-9880f5eebd181541402833566-United-Colors-of-Benetton-Men-Sweatshirts-1271541402833444-3.jpg",
-      "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/7579188/2018/11/5/66490b64-32de-44b4-a6e4-fe36f1c040051541402833548-United-Colors-of-Benetton-Men-Sweatshirts-1271541402833444-4.jpg",
-      "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/7579188/2018/11/5/957be784-7c5d-4e90-ab9f-0928015b22891541402833645-United-Colors-of-Benetton-Men-Sweatshirts-1271541402833444-5.jpg"
-    ],
-    "description": "Navy solid sweatshirt with patchwork, has a round neck, long sleeves, straight hem",
-    "size": [
-      1,
-      1,
-      0,
-      1,
-      0
-    ],
-    "isAccessory": false,
-    "brand": "United Colors of Benetton",
-    "price": 2599
-}
+$(document).ready(function() {
+  var playlistData = [
+    {
+      "id": "1",
+      "title": "Croissants | Flour and Stone",
+      "thumbnail": "https://i.vimeocdn.com/video/600595198_390x220.webp"
+    },
+    {
+      "id": "2",
+      "title": "Perfect Mashed Potatoes and Gravy",
+      "thumbnail": "https://i.vimeocdn.com/video/604150056_390x220.webp"
+    },
+    {
+      "id": "3",
+      "title": "The Heart of Soba",
+      "thumbnail": "https://i.vimeocdn.com/video/726986673_390x220.webp"
+    },
+    {
+      "id": "4",
+      "title": "Grilled Cheese 9 Ways",
+      "thumbnail": "https://i.vimeocdn.com/video/570486309_390x220.webp"
+    },
+    {
+      "id": "5",
+      "title": "Pineapple Cheesecake",
+      "thumbnail": "https://i.vimeocdn.com/video/602705517_390x220.webp"
+    },
+    {
+      "id": "6",
+      "title": "Lemony Chicken Noodle Soup",
+      "thumbnail": "https://i.vimeocdn.com/video/537261616_390x220.jpg"
+    },
+    {
+      "id": "7",
+      "title": "Pumpkin Roll",
+      "thumbnail": "https://i.vimeocdn.com/video/608805594_590x332.jpg"
+    },
+    {
+      "id": "8",
+      "title": "How to Brine a Turkey",
+      "thumbnail": "https://i.vimeocdn.com/video/601730519_590x332.jpg"
+    },
+    {
+      "id": "9",
+      "title": "Stop Motion Dry-Brined Turkey Recipe",
+      "thumbnail": "https://i.vimeocdn.com/video/456852083_590x332.jpg"
+    },
+    {
+      "id": "10",
+      "title": "Butternut Squash Ravioli",
+      "thumbnail": "https://i.vimeocdn.com/video/600328152_590x332.jpg"
+    }
+  ]
+  
+  
+  var videoPlaySectionData = [
+    {
+      "id": "1",
+      "title": "Croissants | Flour and Stone",
+      "description": "There is no other way but to commit wholeheartedly to a relationship with a croissant. We have all found ourselves at the mercy of its allure. Here, in another epic film by the uber talented Nathan Rodger, our Erin divulges her personal romance with The Croissant.",
+      "views": 22500,
+      "vimeoId": 190062231,
+      "isLiked": "true",
+      "isSaved": "true"
+    },
+    {
+      "id": "2",
+      "title": "Perfect Mashed Potatoes and Gravy",
+      "description": "Get the recipe for potatoes at http://bit.ly/mashedpotatoesandgravy.",
+      "views": 22200,
+      "vimeoId": 192650599,
+      "isLiked": "false",
+      "isSaved": "false"
+    },
+    {
+      "id": "3",
+      "title": "The Heart of Soba",
+      "description": "Binge more: MOONCAKE: THE LOST ART - foodtalkies.com/mooncake THE GIFT OF MOCHI - foodtalkies.com/mochi NIRVANA - foodtalkies.com/nirvana EARTH - foodtalkies.com/earth Special thanks to: Mutsuko Soma & Tiffany Ran. Direction, Cinematography & Edit: Andrew Gooi - andrewgooi.com Seattle-based chef and soba artisan, Mutsuko Soma, of Kamonegi / kamonegiseattle.com / shares the craft behind these amazing buckwheat noodles.",
+      "views": 6297820,
+      "vimeoId": 192834629,
+      "isLiked": "false",
+      "isSaved": "true"
+    },
+    {
+      "id": "4",
+      "title": "Grilled Cheese 9 Ways",
+      "description": "The best grilled cheese sandwich recipes clubbed into one video. Cheers!!",
+      "views": 38600,
+      "vimeoId": 166409731,
+      "isLiked": "false",
+      "isSaved": "true"
+    },
+    {
+      "id": "5",
+      "title": "Pineapple Cheesecake",
+      "description": "Pineapple Cheesecake: 150g butter snap cookies; 90g butter, melted; 2 packs cream cheese, room temperature; 3/4 cup sugar; 1/2 tsp vanilla extract; 4 eggs; 4 slices canned pineapple (drained first); Preheat oven at 320°F. Lightly grease mini muffin pans with melted butter, then line each recess with strips of wax paper (1in wide x 3in long), crossing at base. Pulse cookies in a food processor until finely crumbled, then add melted butter to combine. Divide mixture into muffin pans and press firmly into bases. Prepare filling by mixing cream cheese, sugar and vanilla extract with an electric mixer. Add eggs, one at a time, beating well after each addition. Pulse sliced pineapple in a food processor into pulp and add to cream cheese mixture. Divide filling into muffin pans and bake 25-30 minutes until just firm. Cool slightly, then chill for at least 2 hours. Serving suggestion: Pipe some whipped cream over the cheesecake and top with a small slice of pineapple that's been dipped in Li-hing powder. Garnish with mint leaves.",
+      "views": 998110,
+      "vimeoId": 191626548,
+      "isLiked": true,
+      "isSaved": true
+    },
+    {
+      "id": "6",
+      "title": "Lemony Chicken Noodle Soup",
+      "description": "blog.moonberry.com/recipe-lemony-chicken-noodle-soup/",
+      "views": 30210,
+      "vimeoId": 140657270,
+      "isLiked": true,
+      "isSaved": true
+    },
+    {
+      "id": "7",
+      "title": "Pumpkin cream cheese swiss roll",
+      "description": "The ultimate Pumpkin cream cheese swiss roll recipe. Really simple to follow.",
+      "views": 19829,
+      "vimeoId": 196351090,
+      "isLiked": true,
+      "isSaved": true
+    },
+    {
+      "id": "8",
+      "title": "How to Brine a Turkey",
+      "description": "Get the recipe at http://bit.ly/tbrine.",
+      "views": 12230,
+      "vimeoId": 190907060,
+      "isLiked": true,
+      "isSaved": true
+    },
+    {
+      "id": "9",
+      "title": "Stop Motion Dry-Brined Turkey Recipe",
+      "description": "This is a short stop motion video of a recipe I created for a turkey cook-off(wine pairing towards the end). With a special thanks to City Winery Chicago. *Background song - 'Go Bang' by SBTRKT",
+      "views": 30010,
+      "vimeoId": 80687436,
+      "isLiked": true,
+      "isSaved": true
+    },
+    {
+      "id": "10",
+      "title": "Butternut Squash Ravioli with Brown Butter Sage Sauce",
+      "description": "Get the recipe at http://bit.ly/ButternutRav.",
+      "views": 851029,
+      "vimeoId": 189860660,
+      "isLiked": true,
+      "isSaved": true
+    }
+  ]
 
-var product_Title = productData["name"];
-var product_brand = productData["brand"];
-var product_Desc = productData["description"];
-var product_Price = productData["price"];
+  
+  var playlistWrapper = $("#playlist-wrapper");
+  var playerWrapper = $("#player-wrapper");
+  
+  
 
-document.getElementById("name").innerHTML = product_Title;
-document.getElementById("brand").innerHTML = product_brand;
-document.getElementById("description").innerHTML = product_Desc;
-document.getElementById("price").innerHTML = product_Price;
+  function createPlaylistCard(obj) {
+      // <div id="card3" class="playlist-card">
+      //     <img class="thumbnail" src="https://i.vimeocdn.com/video/726986673_390x220.webp" />
+      //     <h3 class="video-card-title">The Heart of Soba</h3>
+      // </div>
 
+      var mainDiv = document.createElement('div');
+      mainDiv.id = obj.id;
+      mainDiv.className = 'playlist-card';
 
-var mainProduct_img_left = document.getElementById("productImg");
+      $(mainDiv).removeClass("active-card");
 
+      var thumbnail = document.createElement('img');
+      thumbnail.src = obj.thumbnail;
+      thumbnail.className = 'thumbnail';
 
+      var title = document.createElement('h3');
+      title.className = 'video-card-title';
+      title.innerHTML = obj.title;
 
-/*
-    Try to change the Image on click event.
-    https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/7579188/2018/11/5/08a7b230-ee8f-46c0-a945-4e835a3c01c01541402833619-United-Colors-of-Benetton-Men-Sweatshirts-1271541402833444-1.jpg
+      mainDiv.appendChild(thumbnail);
+      mainDiv.appendChild(title);
+      // console.log(mainDiv);
 
-*/
-mainProduct_img_left.src =  document.getElementById("img0").src;
-document.getElementById("img0").className="active";
+      $(mainDiv).click(function(){
+        console.log("clicked  !!" + this);
+        $(this).addClass("active-card");
+        
+        
+        //add class active.
 
-document.getElementById("img0").addEventListener("click", function() {
-    mainProduct_img_left.src =  document.getElementById("img0").src;
-    document.getElementById("img0").className="active";
-    document.getElementById("img1").classList.remove("active");
-    document.getElementById("img2").classList.remove("active");
-    document.getElementById("img3").classList.remove("active");
-    document.getElementById("img5").classList.remove("active");
-  });
-  document.getElementById("img1").addEventListener("click", function() {
-    mainProduct_img_left.src =  document.getElementById("img1").src;
-    document.getElementById("img1").className="active";
-    document.getElementById("img0").classList.remove("active");
-    document.getElementById("img2").classList.remove("active");
-    document.getElementById("img3").classList.remove("active");
-    document.getElementById("img5").classList.remove("active");
-  });
-  document.getElementById("img2").addEventListener("click", function() {
-    mainProduct_img_left.src =  document.getElementById("img2").src;
-    document.getElementById("img2").className="active";
-    document.getElementById("img0").classList.remove("active");
-    document.getElementById("img1").classList.remove("active");
-    document.getElementById("img4").classList.remove("active");
-    document.getElementById("img3").classList.remove("active");
-    document.getElementById("img5").classList.remove("active");
-  });
-  document.getElementById("img3").addEventListener("click", function() {
-    mainProduct_img_left.src =  document.getElementById("img3").src;
-    document.getElementById("img3").className="active";
-    document.getElementById("img0").classList.remove("active");
-    document.getElementById("img1").classList.remove("active");
-    document.getElementById("img2").classList.remove("active");
-    document.getElementById("img4").classList.remove("active");
-    document.getElementById("img5").classList.remove("active");
-  });
+        // load that playlist video in video player
 
-  document.getElementById("img4").addEventListener("click", function() {
-    mainProduct_img_left.src =  document.getElementById("img4").src;
-    document.getElementById("img4").className="active";
-    document.getElementById("img0").classList.remove("active");
-    document.getElementById("img1").classList.remove("active");
-    document.getElementById("img2").classList.remove("active");
-    document.getElementById("img3").classList.remove("active");
-    document.getElementById("img5").classList.remove("active");
-  });
+        var videoPlayerToLoad = videoPlaySectionData[parseInt(this.id)-1];
 
+        // console.log(videoPlayerToLoad);
+        $(this).addClass("active-card");
+        //https://player.vimeo.com/video/190062231"
+        var videoPlayer = $("#video-player");
+        videoPlayer.attr("src","https://player.vimeo.com/video/"  + videoPlayerToLoad.vimeoId);
 
-  document.getElementById("img5").addEventListener("click", function() {
-    mainProduct_img_left.src =  document.getElementById("img5").src;
-    document.getElementById("img5").className="active";
-    document.getElementById("img0").classList.remove("active");
-    document.getElementById("img1").classList.remove("active");
-    document.getElementById("img2").classList.remove("active");
-    document.getElementById("img3").classList.remove("active");
-    document.getElementById("img4").classList.remove("active");
+        var viewsCount = $("#views-count");
+        viewsCount.html(videoPlayerToLoad.views);
 
-    console.log("")
-  });
+        var videoTitle = $("#video-title");
+        videoTitle.html(videoPlayerToLoad.title);
+
+        var videoDesc = $("#video-description");
+        videoDesc.html(videoPlayerToLoad.description);
+        //click functionality on like and book mark.
+        
+      })
+     
+      
+      playlistWrapper.append(mainDiv);
+  }
+  
+  for(var i = 0; i < playlistData.length;i++){ 
+    // console.log(createPlaylistCard(playlistData[i]).title);
+    createPlaylistCard(playlistData[i])
+    
+  }
 
 
-
-
-
+});
